74f40fd047930da78e10da3944d547c8526194b9
Andrew Carter <acarter@cs.hmc.edu>
2012-11-03 02:31:47 -0700

    Terrain!!!

---
+++ tbg/terrain/base.py
@@ -0,0 +1,38 @@
+import pygame
+from pygame.locals import *
+import os
+loaded = {}
+
+COLORS = ((0,0,250), (0,250,0), (250,250,0))
+
+class Base(pygame.sprite.Sprite):
+  def __init__(self, loc, cost, board, image, **kwargs):
+    super(Base, self).__init__(**kwargs)
+    self.cost = cost
+    self.board = board
+    if image not in loaded:
+      loaded[image] = pygame.image.load(os.path.join("assets", image))
+    self.image = loaded[image]
+    self.rect = pygame.rect.Rect(board.pos(loc), self.image.get_size())
+    self.loc = loc
+    self.clear()
+  def clear(self):
+    self.over = [False, False, False]
+  def enter(self, num):
+    self.over[num] = True
+  def select(self):
+    self.enter(0)
+  def movable(self):
+    self.enter(1)
+  def attackable(self):
+    self.enter(2)
+  def overlay(self, screen):
+    for i,v in enumerate(self.over):
+      if v: break
+    else: return
+    color = COLORS[i]
+    select = pygame.Surface(self.board.div)
+    select.convert()
+    select.fill(color)
+    screen.blit(select, self.board.pos(self.loc), special_flags = BLEND_ADD)
+    return i
