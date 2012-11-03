e07aaf41b9bd1c34c7f993cc49972fd8e21452f0
Andrew Carter <acarter@cs.hmc.edu>
2012-11-03 04:54:41 -0700

    Very sandy

--- tbg/terrain/sand.py
+++ tbg/terrain/sand.py
@@ -5,4 +5,4 @@ from .base import Base
 
 class Sand(Base):
   def __init__(self, **kwargs):
-    super(Sand, self).__init__(cost=2,image="sand.png",**kwargs)
+    super(Sand, self).__init__(cost=2,image=os.path.join("terrain","sand.png"),**kwargs)
