72652187a2e151df727b98a9685f47711d4d8c04
Josh Oratz <joratz@hmc.edu>
2012-11-03 07:30:13 -0700

    fixing

--- whackamullet.py
+++ whackamullet.py
@@ -26,13 +26,28 @@ def load_sliced_sprites(self, w, h, filename):
     	images.append(master_image.subsurface((i*w,0,w,h)))
     return images,images[0].get_rect()
 
+class Eyes(pygame.sprite.Sprite):
+    def __init__(self,x,y,i):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_image('xeyes.png',-1)
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        self.rect.topleft = 150 + x*200,125 + y*150
+        self.timer = 0
+        self.done = False
+        self.index = i
+    def update(self):
+        self.timer += 1
+        if self.timer > 30:
+            self.done = True
+
 class Mullet(pygame.sprite.Sprite):
     def __init__(self,x,y,i):
         pygame.sprite.Sprite.__init__(self)
         self.image, self.rect = load_image('mulletWHACK.png',-1)
         screen = pygame.display.get_surface()
         self.area = screen.get_rect()
-        self.rect.topleft = 150 + x*(self.image.get_width() + 100),125 + y*(50 + self.image.get_height())
+        self.rect.topleft = 150 + x*200,125 + y*150
         self.timer = 0
         self.done = False
         self.index = i
@@ -54,11 +69,13 @@ class WhackAMullet(object):
         self.mullets = [0,0,0,0,0,0,0,0,0]
         self.mulletlist = []
         self.counter = 0
+        self.eyeslist = []
     def reset(self):
         self.counter = 0
         self.mullets = [0,0,0,0,0,0,0,0,0]
         self.score = 0
         self.mulletlist = []
+        self.eyeslist = []
         return True
     def update(self, **kwargs):
         self.counter += 1
@@ -85,16 +102,24 @@ class WhackAMullet(object):
                 for mullet in self.mulletlist:
                     if mullet.rect.collidepoint(pygame.mouse.get_pos()):
                         self.score += 100
-                        self.mullets[mullet.index] = 0
+                        self.mullets[mullet.index] = 2
+                        self.eyeslist.append(Eyes(mullet.index % 3, mullet.index / 3, mullet.index))
                         self.mulletlist.remove(mullet)
             for mullet in self.mulletlist:
-                mullet.update()          
+                mullet.update()
+        for eye in self.eyeslist:
+            eye.update()
+            if eye.done:
+                self.mullets[eye.index] = 0
+                self.eyeslist.remove(eye)
 
     def draw(self, **kwargs):
         self.screen.blit(self.background, (0,0))
         self.screen.blit(self.back, self.backRect)
         allsprites = pygame.sprite.RenderPlain(self.mulletlist)
+        allsprites2 = pygame.sprite.RenderPlain(self.eyeslist)
         allsprites.draw(self.screen)
+        allsprites2.draw(self.screen)
         pygame.display.flip()
         
     
