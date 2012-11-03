d0aa480af8f48883282994481b0b91f29b371d51
Andrew Carter <acarter@cs.hmc.edu>
2012-11-03 00:40:24 -0700

    Moving unit code intu units/

--- tbg/board.py
+++ tbg/board.py
@@ -4,6 +4,7 @@ from pygame.locals import *
 from sys import exit
 from .io.mouse import Mouse
 import os
+from .units.dude import Dude
 WIDTH = 0
 HEIGHT= 1
 START = ((0,0), (1,1), (3,2))
@@ -16,8 +17,8 @@ class Board(object):
     self.mouse  = Mouse()
     self.selected = None
     self.div = div
-    self.eles = dict( (loc, Elephant(tuple(a * b for (a,b) in zip(loc,div)))) for loc in START)
-    self.sprites = pygame.sprite.RenderPlain(tuple(self.eles.values()))
+    self.units = dict( (loc, Dude(loc=loc,board=self)) for loc in START)
+    self.sprites = pygame.sprite.RenderPlain(tuple(self.units.values()))
   def genBackground(self, div):
     self.background = pygame.Surface(self.screen.get_size())
     self.background = self.background.convert()
@@ -29,44 +30,34 @@ class Board(object):
       pygame.draw.line(self.background, (0,0,0), (0, i), (size[0], i))
   def update(self, **kwargs):
     self.mouse.tick()
-    pos = self.snap(self.mouse.pos)
-    loc = self.loc(pos)
+    loc = self.loc(self.mouse.pos)
     for i in range(0, 2):
       if self.mouse.clicked(i):
-        if self.selected == pos:
+        if self.selected == loc:
           self.selected = None
-        elif self.selected is None and loc in self.eles:
-          self.selected = pos
-        elif loc in self.eles:
+        elif self.selected is None and loc in self.units:
+          self.selected = loc
+        elif loc in self.units:
           return True
         elif self.selected:
-          self.eles[self.loc(self.selected)].move(pos)
-          self.eles[loc] = self.eles[self.loc(self.selected)]
-          del self.eles[self.loc(self.selected)]
+          self.units[self.selected].move(loc)
+          self.units[loc] = self.units[self.selected]
+          del self.units[self.selected]
           self.selected = None
-        else:
-          print("No elephant")
           
     self.sprites.update()
   def snap(self, pos):
     return tuple(i - (i % j) for (i, j) in zip(pos, self.div))
   def loc(self, pos):
     return tuple(i // j for (i, j) in zip(pos, self.div))
+  def pos(self, loc):
+    return tuple(i * j for (i,j) in zip(loc, self.div))
   def draw(self, **kwargs):
     self.screen.blit(self.background, (0,0))
     if self.selected:
       select = pygame.Surface(self.div)
       select = select.convert()
       select.fill((250,0,0))
-      self.screen.blit(select, self.snap(self.selected))
+      self.screen.blit(select, self.pos(self.selected))
     self.sprites.draw(self.screen)
 
-class Elephant(pygame.sprite.Sprite):
-  image = pygame.image.load(os.path.join("assets", "andrewdrewanelephant.png"))
-  def __init__(self, loc):
-    super(Elephant,self).__init__()
-    self.rect = pygame.rect.Rect(loc, self.image.get_size())
-  def update(self):
-    super(Elephant,self).update()
-  def move(self, loc):
-    self.rect.topleft = loc
---
+++ tbg/units/dude.py
@@ -0,0 +1,5 @@
+from .unit import Unit
+class Dude(Unit):
+  def __init__(self, **kwargs):
+    super(Dude, self).__init__(image="andrewdrewanelephant.png", **kwargs)
+  
